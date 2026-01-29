d6384f078c48e9db0f035d21466e1e2c2f45983e
unknown <dev@DSN1191348072.edu119.local>
2026-01-29 10:57:05 -0300

    ola

---
+++ java/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "raul";
+        System.out.printf("Hello,"+nome+"!");
+        System.out.printf("hello my brother %S!",nome);
+
+        int numero = 5;
+        float numero2 = 2.6F;
+        double numero3 = 2.9;
+        long numero4 = 89_898_989L;
+
+
+        System.out.printf("%.3f\n",numero4 * numero3);
+    }
+}
